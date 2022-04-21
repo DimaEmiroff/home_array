f2f628b97a4575082d59830bbbe51a0624dc34db
dimonEmiroff <mr-tvister@mail.ru>
2022-04-21 08:05:23 +0300

    commit first

---
+++ home_pop_shift.js
@@ -0,0 +1,13 @@
+//Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент.
+// let arr = ['js', 'css', 'jq'];
+// let prin = arr.shift(1, 2)
+// console.log(prin)
+// let prin1 = arr.slice(0,1);
+// console.log(prin1)
+
+//Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
+// let arr = ['js', 'css', 'jq'];
+// let prin2 = arr.pop(0,1);
+// console.log(prin2)
+
+
