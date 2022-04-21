b53db9d67d84f4444ef7a9b68dee47f3ba771717
dimonEmiroff <mr-tvister@mail.ru>
2022-04-21 08:02:42 +0300

    commit first

---
+++ home_merge.js
@@ -0,0 +1,11 @@
+//Даны два массива: ['a', 'b', 'c'] и [1, 2, 3]. Объедините их вместе.
+// let arr = [1, 2, 3];
+// let arr1 = [,'a', 'b', 'c'];
+// arr.push()
+// let isSum = arr + arr1
+// console.log(isSum)
+
+//Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+// let arr = [1, 2, 3];
+// arr.reverse()
+// console.log(arr)
